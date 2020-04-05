77d0bd4d4c93905e0f9952b4849b9a69b100491b
KaitoKuraba <dairunnerz@gmail.com>
2020-04-05 19:51:50 +0700

    This is a final one
    sent assignment exercise4

---
+++ foundation_english = 70.py
@@ -0,0 +1,10 @@
+foundation_english = 70
+general_Business = 75.4
+intro_Com_system = 68.4
+comprog = 95.6
+
+print("---Your Score----")
+print("Foundation English: ", foundation_english)
+print("General Business: ", general_Business)
+print("Introduction to Computer system: ", intro_Com_system)
+print("Computer Programming: ", comprog)
